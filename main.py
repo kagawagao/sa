faded355f6ecb41c96d6de2afda6737c6e63197a
kagawagao <kingsongao1221@gmail.com>
2023-12-07 23:10:37 +0800

    feat: add cors support

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from api import api_router
 from utils.log import logger
 
 app = FastAPI(title="Segment Anything Model Service")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api_router)
 
