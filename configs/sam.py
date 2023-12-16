38dfa89bfb89e39b33407e598e5ad94627166b4b
kagawagao <kingsongao1221@gmail.com>
2023-12-16 09:36:45 +0800

    refactor: use env default vars

--- configs/sam.py
+++ configs/sam.py
@@ -6,11 +6,11 @@ load_dotenv()
 
 work_dir = os.getcwd()
 
-model_path: str = os.getenv('SAM_MODEL_CHECKPOINT') or 'sam_vit_h_4b8939.pth'
+model_path: str = os.getenv('SAM_MODEL_CHECKPOINT', 'sam_vit_h_4b8939.pth')
 
 # SAM 模型地址
 SAM_MODEL_CHECKPOINT = model_path.startswith(
     '/') and model_path or os.path.join(work_dir, model_path)
 
 # SAM 模型类型
-SAM_MODEL_TYPE = os.getenv('SAM_MODEL_TYPE') or 'vit_h'
+SAM_MODEL_TYPE = os.getenv('SAM_MODEL_TYPE', 'vit_h')
